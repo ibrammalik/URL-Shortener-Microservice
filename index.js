ecd5f7c3bea3f13695bdfcd1bdd80c186f8ecb6b
ibrammalik <malikmaul1173@gmail.com>
2022-08-05 13:25:11 +0700

    Passed The Challange, Updated

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+const dns = require("dns");
 const app = express();
+
+//set up bodyParse package
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+//use encoding in password for special characters
+//connect to database
+mongoose.connect(process.env["MONGO_URI"], { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+//creating new schema
+let urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String,
+});
+
+//accessing model
+let modelURL = mongoose.model("modelURL", urlSchema);
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+//short url api
+app.post("/api/shorturl", (req, res) => {
+  let original_url = req.body.url;
+  let short_url = shortid.generate();
+  let urlHostname = () => {
+    const matches = original_url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    return matches && matches[1];
+  };
+
+  dns.lookup(urlHostname(), (err, addresses, family) => {
+    if (err || urlHostname() === null) {
+      res.send({ "error": 'invalid url' });
+    } else {
+      modelURL.create({ original_url: original_url, short_url: short_url }, (err, data) => {
+        if (err) return handleError(err);
+        console.log("Saved! ->" + data);
+        // saved!
+      });
+      res.json({ original_url, short_url });
+    }
+  });
+
+});
+
+//
+app.get("/api/shorturl/:id?", (req, res) => {
+  modelURL.findOne({ short_url: req.params.id }, (err, data) => {
+    if (err) return handleError(err);
+    else if (data === null) {
+      res.json({ "error": "No short URL found for the given input" });
+    } else {
+      res.redirect(data.original_url);
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
